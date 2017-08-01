7dbae8628b7958ffdbe3318808a6275644e5c7c6
Sergey Lyskov <sergey.lyskov@jhu.edu>
2017-08-01 16:39:48 -0400

    Fixing -j option default and description

--- build.py
+++ build.py
@@ -152,7 +152,7 @@ def main(args):
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-j', '--jobs', default=1, type=int, help="Number of processors to use on when building. (default: use all avaliable memory)")
+    parser.add_argument('-j', '--jobs', default=1, const=0, nargs="?", type=int, help="Number of processors to use on when building, use '-j' with no arguments to launch job-per-core. (default: 1) ")
     parser.add_argument("--type", default='Release', choices=['Release', 'Debug', 'MinSizeRel', 'RelWithDebInfo'], help="Specify build type")
     parser.add_argument('--compiler', default='clang', help='Compiler to use, defualt is clang')
     parser.add_argument('--binder', default='', help='Path to Binder tool. If none is given then download, build and install binder into main/source/build. Use "--binder-debug" to control which mode of binder (debug/release) is used.')
