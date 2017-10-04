d72b00d490af583dd06ea82c05fe8b67b21f4637
Alex Ford <fordas@uw.edu>
2017-10-04 11:37:19 -0700

    Add test for include_for_class configuration.

---
+++ test/T31.include_for_class.hpp
@@ -0,0 +1,12 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// Copyright (c) 2016 Sergey Lyskov <sergey.lyskov@jhu.edu>
+//
+// All rights reserved. Use of this source code is governed by a
+// MIT license that can be found in the LICENSE file.
+
+#pragma once
+
+#include <T31.include_for_class.incl.a.include>
+#include <T31.include_for_class.incl.b.include>
