c949b3cf71700187d6a6e8a3758487233e020e9b
Danny Farrell <danpf@uw.edu>
2018-06-16 07:27:25 -0700

    Added example for cmake and bash

---
+++ examples/example_struct/include/test_struct/test_struct.hpp
@@ -0,0 +1,41 @@
+
+
+#ifndef TEST_STRUCT_H
+#define TEST_STRUCT_H
+
+#include <string>
+#include <vector>
+
+namespace testers {
+struct test_my_struct {
+	int an_int;
+	std::string a_string;
+	std::vector<int> a_vector;
+	float a_float;
+
+	test_my_struct() {
+		an_int = 27;
+		a_string = "TEST_STRING";
+		a_vector = std::vector<int>{1,2,3,4,5};
+		a_float = 88.88;
+	}
+
+	void
+	increment_int() {
+		++an_int;
+	}
+
+	void
+	add_float() {
+		a_float += 22.22;
+	}
+
+	void
+	append_vec() {
+		a_vector.push_back(a_vector.back()+1);
+	}
+
+};
+}
+
+#endif
---
+++ examples/example_struct/make_bindings_via_cmake.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import sys
+import shutil
+import subprocess
+from distutils.sysconfig import get_python_inc
+
+def make_all_includes():
+    all_includes = []
+    all_include_filename = "all_cmake_includes.hpp"
+    for filename in glob.glob("include/**/*.hpp", recursive=True):
+        with open(filename, 'r') as fh:
+            for line in fh:
+                if line.startswith("#include"):
+                    all_includes.append(line.strip())
+    all_includes = list(set(all_includes))
+    with open(all_include_filename, 'w') as fh:
+        for include in all_includes:
+            fh.write(f"{include}\n")
+    return all_include_filename
+
+
+def make_bindings_code(all_includes_fn):
+    bindings_dir = "cmake_bindings"
+    shutil.rmtree(bindings_dir, ignore_errors=True)
+    os.mkdir(bindings_dir)
+    binder_executable = glob.glob(f"{os.getcwd()}/../../build/llvm-4.0.0/build_4.0.0*/bin/binder")[0]
+    command = f'{binder_executable} --root-module test_struct --prefix {os.getcwd()}/{bindings_dir}/ --bind testers -trace {all_includes_fn}  -- -std=c++11 -I{os.getcwd()}/include -DMY_PROJECT_DEFINE -DNDEBUG'.split()
+    print(" ".join(command))
+    ret = subprocess.call(command)
+    sources_to_compile = []
+    with open(f"{bindings_dir}/test_struct.sources", 'r') as fh:
+        for line in fh:
+            sources_to_compile.append(line.strip())
+    return sources_to_compile
+
+
+def compile_sources(sources_to_compile):
+    bindings_dir = "cmake_bindings"
+    og_dir = os.getcwd()
+    os.chdir(bindings_dir)
+    lines_to_write = []
+    lines_to_write.append("project(test_struct)")
+
+    binder_source = f"{os.getcwd()}/../../../source"
+    this_project_source = f"{os.getcwd()}/../include"
+    pybind_source = f"{os.getcwd()}/../../../build/pybind11/include"
+    compiled_sources = []
+    include_directories = []
+    for include_dir in [binder_source, this_project_source, pybind_source, get_python_inc()]:
+        lines_to_write.append(f"include_directories({include_dir})")
+    lines_to_write.append("set_property(GLOBAL PROPERTY POSITION_INDEPENDENT_CODE ON)")
+    lines_to_write.append("add_definitions(-DNDEBUG)")
+
+    lines_to_write.append("add_library(test_struct SHARED")
+    for source in sources_to_compile:
+        lines_to_write.append(f"\t{source}")
+    lines_to_write.append(")")
+    lines_to_write.append('set_target_properties(test_struct PROPERTIES PREFIX "")')
+    lines_to_write.append("set_target_properties(test_struct PROPERTIES SUFFIX \".so\")")
+
+    with open("CMakeLists.txt", 'w') as f:
+        for line in lines_to_write:
+            f.write(f"{line}\n")
+    # Done making CMakeLists.txt
+    subprocess.call("cmake -G Ninja".split())
+    subprocess.call("ninja")
+    sys.path.append('.')
+    import test_struct
+    test_obj = test_struct.testers.test_my_struct()
+    print(test_obj.an_int)
+
+
+
+def main():
+   all_includes_fn =  make_all_includes()
+   sources_to_compile = make_bindings_code(all_includes_fn)
+   compile_sources(sources_to_compile)
+#g++ \
+#  -O3 \
+#  -I"${pybase::-12}"/include/python3.6m -I$PWD/../../../build/pybind11/include -I$PWD/../include \
+#  -I$PWD/../../../source -shared  \
+#  -std=c++11  -c test_struct.cpp  \
+#  -o test_struct.o -fPIC
+
+
+if __name__ == "__main__":
+    main()
+
+# # Make bindings code 
+#$PWD/../../build/llvm-4.0.0/build_4.0.0*/bin/binder \
+#  --root-module test_struct \
+#  --prefix $PWD/bash_bindings/ \
+#  --bind "" -trace \
+#  all_bash_includes.hpp \
+#  -- -std=c++11 -I$PWD/include \
+#  -DMY_PROJECT_DEFINE -DNDEBUG
+#
+## compile first generated bindings
+## NOTE 'pybase' may need to change on your system
+#cd bash_bindings
+#pybase=`which python3`
+#g++ \
+#  -O3 \
+#  -I"${pybase::-12}"/include/python3.6m -I$PWD/../../../build/pybind11/include -I$PWD/../include \
+#  -I$PWD/../../../source -shared  \
+#  -std=c++11  -c test_struct.cpp  \
+#  -o test_struct.o -fPIC
+#
+#
+#
+## compile second generated bindings
+## NOTE 'pybase' may need to change on your system
+#cd test_struct/
+#g++ \
+#  -O3 \
+#  -I"${pybase::-12}"/include/python3.6m -I$PWD/../../../../build/pybind11/include -I$PWD/..//../include \
+#  -I$PWD/../../../../source -shared  \
+#  -std=c++11  -c test_struct.cpp  \
+#  -o test_struct.o -fPIC && \
+#
+#cd ../
+#
+## Link together compiled bindings
+#g++ -o test_struct.so -shared test_struct/test_struct.o test_struct.o
+#
+## Try running via python
+#python3 -c "import sys; sys.path.append('.'); import test_struct; f = test_struct.testers.test_my_struct(); print(f.an_int)"
+#
+#cd ../
