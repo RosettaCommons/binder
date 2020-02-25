a597984c3a94627f3ac25ede0e38d8045fcee4fb
Andrii Verbytskyi <andriish@hermes.mpp.mpg.de>
2020-02-25 01:42:49 +0100

    More CI

--- .github/workflows/fedora31/entrypoint.sh
+++ .github/workflows/fedora31/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -l
 uname -a 
 cat /etc/issue
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
-yum -y install  cmake make gcc gcc-c++
+yum -y install  libcxx-devel cmake make gcc gcc-c++
 cmake CMakeLists.txt
 make
 
