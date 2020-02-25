9379558c236ec12645b84568fc7bb8952c99627e
Andrii Verbytskyi <andriish@hermes.mpp.mpg.de>
2020-02-25 02:06:28 +0100

    More CI

--- .github/workflows/centos8/entrypoint.sh
+++ .github/workflows/centos8/entrypoint.sh
@@ -4,7 +4,7 @@ cat /etc/issue
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
 yum -y install  cmake make gcc gcc-c++
-cmake CMakeLists.txt
+cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config
 make
 
 out=$?
