775d20e69a2e8a73ffcb34a2f7b86aebd73e3463
Andrii Verbytskyi <andriish@hermes.mpp.mpg.de>
2020-02-28 23:30:44 +0100

    Fix

--- .github/workflows/centos8/entrypoint.sh
+++ .github/workflows/centos8/entrypoint.sh
@@ -3,7 +3,7 @@ uname -a
 cat /etc/issue
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
-yum -y install  cmake make gcc gcc-c++
+yum -y install  cmake make gcc gcc-c++ diffutils
 rpm -Uvh https://download.copr.fedorainfracloud.org/results/averbyts/fastjet/epel-8-x86_64/01259648-pybind11/pybind11-devel-2.4.3-2.el8.x86_64.rpm
 yum -y install  python3 python3-devel python2 python2-devel
 cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config -DBINDER_MOCK_TEST=OFF
