14bc460ac47b4e259ff60e7b968bb8097ee00ea9
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-02-28 20:43:58 +0100

    Start to implement tests in Cmake

--- .github/workflows/centos7/entrypoint.sh
+++ .github/workflows/centos7/entrypoint.sh
@@ -4,6 +4,7 @@ cat /etc/issue
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
 yum -y install  libcxx-devel cmake3 make gcc gcc-c++
+rpm -Uvh https://download.copr.fedorainfracloud.org/results/averbyts/fastjet/epel-7-x86_64/01259648-pybind11/pybind11-devel-2.4.3-2.el7.x86_64.rpm
 cmake3 CMakeLists.txt
 make
 
--- .github/workflows/fedora31/entrypoint.sh
+++ .github/workflows/fedora31/entrypoint.sh
@@ -3,6 +3,7 @@ uname -a
 cat /etc/issue
 yum -y install  clang clang-devel clang-libs  llvm llvm-devel llvm-static
 yum -y install  libcxx-devel cmake make gcc gcc-c++
+yum -y install  pybind11-devel
 cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config
 make
 
