17eb4f87eb0956afd5e4a73aa9a471773b550a40
Andrii Verbytskyi <andriish@hermes.mpp.mpg.de>
2020-02-28 22:57:49 +0100

    Fix

--- .github/workflows/centos8/entrypoint.sh
+++ .github/workflows/centos8/entrypoint.sh
@@ -6,8 +6,9 @@ yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
 yum -y install  cmake make gcc gcc-c++
 rpm -Uvh https://download.copr.fedorainfracloud.org/results/averbyts/fastjet/epel-8-x86_64/01259648-pybind11/pybind11-devel-2.4.3-2.el8.x86_64.rpm
 yum -y install  python3 python3-devel python2 python2-devel
-cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config
+cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config -DBINDER_MOCK_TEST=OFF
 make
+ctest .
 
 out=$?
 echo ::set-output name=out::$out
--- .github/workflows/fedora31/entrypoint.sh
+++ .github/workflows/fedora31/entrypoint.sh
@@ -4,8 +4,8 @@ cat /etc/issue
 yum -y install  clang clang-devel clang-libs  llvm llvm-devel llvm-static
 yum -y install  libcxx-devel cmake make gcc gcc-c++
 yum -y install  pybind11-devel python3 python3-devel python2 python2-devel
-cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config
+cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config -DBINDER_MOCK_TEST=OFF
 make
-
+ctest .
 out=$?
 echo ::set-output name=out::$out
