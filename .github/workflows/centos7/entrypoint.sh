68478370348a9b6d3fc0b958a664bb3bcaa74770
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-02-28 21:11:39 +0100

    MOre CI

--- .github/workflows/centos7/entrypoint.sh
+++ .github/workflows/centos7/entrypoint.sh
@@ -5,6 +5,7 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarc
 yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
 yum -y install  libcxx-devel cmake3 make gcc gcc-c++
 rpm -Uvh https://download.copr.fedorainfracloud.org/results/averbyts/fastjet/epel-7-x86_64/01259648-pybind11/pybind11-devel-2.4.3-2.el7.x86_64.rpm
+yum -y install  python3 python3-devel python python-devel
 cmake3 CMakeLists.txt
 make
 
--- .github/workflows/centos8/entrypoint.sh
+++ .github/workflows/centos8/entrypoint.sh
@@ -5,6 +5,7 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarc
 yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
 yum -y install  cmake make gcc gcc-c++
 rpm -Uvh https://download.copr.fedorainfracloud.org/results/averbyts/fastjet/epel-8-x86_64/01259648-pybind11/pybind11-devel-2.4.3-2.el8.x86_64.rpm
+yum -y install  python3 python3-devel python2 python2-devel
 cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config
 make
 
