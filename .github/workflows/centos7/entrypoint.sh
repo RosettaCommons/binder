a0dc162889f67d8637dfbffc510ef261fb213def
Andrii Verbytskyi <andriish@hermes.mpp.mpg.de>
2020-02-29 23:22:51 +0100

    Simplify CI

--- .github/workflows/centos7/entrypoint.sh
+++ .github/workflows/centos7/entrypoint.sh
@@ -6,9 +6,9 @@ yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
 yum -y install  libcxx-devel cmake3 make cmake gcc gcc-c++ diffutils
 rpm -Uvh https://download.copr.fedorainfracloud.org/results/averbyts/fastjet/epel-7-x86_64/01259648-pybind11/pybind11-devel-2.4.3-2.el7.x86_64.rpm
 yum -y install  python3 python3-devel python python-devel diffutils
-cmake3 CMakeLists.txt -DBINDER_MOCK_TEST=OFF   -DLibClang_INCLUDE_DIRS=/usr/lib/clang/3.4.2/include
+cmake3 CMakeLists.txt
 make
-ctest .
+ctest . --output-on-failure 
 
 out=$?
 echo ::set-output name=out::$out
