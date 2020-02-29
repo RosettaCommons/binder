0bba454134c7188abb7a37850c6c43a1e48245c8
Your Name <you@example.com>
2020-02-28 19:23:15 -0500

    OK

--- .github/workflows/centos8/entrypoint.sh
+++ .github/workflows/centos8/entrypoint.sh
@@ -6,7 +6,7 @@ yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
 yum -y install  cmake make gcc gcc-c++ diffutils
 rpm -Uvh https://download.copr.fedorainfracloud.org/results/averbyts/fastjet/epel-8-x86_64/01259648-pybind11/pybind11-devel-2.4.3-2.el8.x86_64.rpm
 yum -y install  python3 python3-devel python2 python2-devel
-cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config -DBINDER_MOCK_TEST=OFF
+cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config -DBINDER_MOCK_TEST=OFF  -DLibClang_INCLUDE_DIRS=/usr/lib64/clang/8.0.1/include
 make
 ctest .
 
--- .github/workflows/fedora31/entrypoint.sh
+++ .github/workflows/fedora31/entrypoint.sh
@@ -4,7 +4,7 @@ cat /etc/issue
 yum -y install  clang clang-devel clang-libs  llvm llvm-devel llvm-static
 yum -y install  libcxx-devel cmake make gcc gcc-c++
 yum -y install  pybind11-devel python3 python3-devel python2 python2-devel diffutils
-cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config -DBINDER_MOCK_TEST=OFF
+cmake CMakeLists.txt -DLLVMCONFIG=/usr/bin/llvm-config -DBINDER_MOCK_TEST=OFF -DLibClang_INCLUDE_DIRS=/usr/lib64/clang/10.0.0/include
 make
 ctest .
 out=$?
