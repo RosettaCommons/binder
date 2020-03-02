3e636207c4fde521ab7984c1a6e6f0b2d7441d68
Andrii Verbytskyi <andriish@hermes.mpp.mpg.de>
2020-03-02 01:15:05 +0100

    FIX ci

--- .github/workflows/centos7/entrypoint.sh
+++ .github/workflows/centos7/entrypoint.sh
@@ -5,7 +5,7 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarc
 yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
 yum -y install  libcxx-devel cmake3 make cmake gcc gcc-c++ 
 yum -y install yum-plugin-copr
-yum -y copr enable averbyts/fastjet
+yum -y copr enable averbyts/pybind11
 yum -y install pybind11-devel
 yum -y install  python3 python3-devel python python-devel 
 cmake3 CMakeLists.txt
--- .github/workflows/centos8/entrypoint.sh
+++ .github/workflows/centos8/entrypoint.sh
@@ -5,7 +5,7 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarc
 yum -y install  clang clang-devel clang-libs  llvm-devel llvm-static
 yum -y install  cmake make gcc gcc-c++ diffutils
 yum -y install yum-plugin-copr
-yum -y copr enable averbyts/fastjet
+yum -y copr enable averbyts/pybind11
 yum -y install pybind11-devel
 yum -y install  python3 python3-devel python2 python2-devel
 cmake CMakeLists.txt
