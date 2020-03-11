40b9d9bd4f691a3896763f6e986afde93be99c9c
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-03-11 18:19:12 +0100

    Build on Ubuntu20

---
+++ .github/workflows/ubuntu20/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+uname -a 
+cat /etc/issue
+apt-get update
+apt-get -y install  clang llvm  clang-dev*
+apt-get -y install  cmake make gcc
+apt-get install  python-pybind11 python3-pybind11    pybind11-dev pybind11-doc python-dev python3-dev
+cmake CMakeLists.txt  -DBINDER_ENABLE_TEST=OFF
+make
+ctest . --output-on-failure 
+out=$?
+echo ::set-output name=out::$out
