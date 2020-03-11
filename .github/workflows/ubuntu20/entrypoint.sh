460abc3cf06316370b9efc5511e828c189828903
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-03-11 18:30:28 +0100

    Build on Ubuntu20

--- .github/workflows/ubuntu20/entrypoint.sh
+++ .github/workflows/ubuntu20/entrypoint.sh
@@ -4,8 +4,8 @@ cat /etc/issue
 apt-get update
 apt-get -y install  clang llvm  clang-dev*
 apt-get -y install  cmake make gcc g++
-apt-get install  python-pybind11 python3-pybind11    pybind11-dev pybind11-doc python-dev python3-dev
-cmake CMakeLists.txt 
+apt-get install  python-pybind11 python3-pybind11    pybind11-dev pybind11-doc python2-dev python3-dev
+cmake CMakeLists.txt -Dpybind11_DIR=/usr/lib/cmake/pybind11/
 make
 ctest . --output-on-failure 
 out=$?
