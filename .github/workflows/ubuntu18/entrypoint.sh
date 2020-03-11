575f95a842fd13f541085308d7d4b68c2a831aa9
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-03-11 17:49:06 +0100

    Build on Ubuntu

--- .github/workflows/ubuntu18/entrypoint.sh
+++ .github/workflows/ubuntu18/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 uname -a 
 cat /etc/issue
-sudo apt-get install  clang llvm libclang clang-dev*
-sudo apt-get install  cmake make gcc gcc-c++
+apt-get install  clang llvm libclang clang-dev*
+apt-get install  cmake make gcc gcc-c++
 #sudo apt-get install  pybind11-devel python3 python3-devel python2 python2-devel 
 cmake CMakeLists.txt -DBINDER_ENABLE_TEST=OFF
 make
