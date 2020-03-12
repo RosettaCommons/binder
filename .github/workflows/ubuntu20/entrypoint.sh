b03cd3be235e9761af1c8cf904bbb5cbdf2ea91e
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-03-12 18:30:40 +0100

    Attempt to have tests on Ubuntu

--- .github/workflows/ubuntu20/entrypoint.sh
+++ .github/workflows/ubuntu20/entrypoint.sh
@@ -6,8 +6,7 @@ apt-get update
 apt-get -y install  clang llvm  clang-dev*
 apt-get -y install  cmake make gcc g++
 apt-get install   python3-pybind11    pybind11-dev pybind11-doc python2-dev python3-dev
-cmake CMakeLists.txt -DBINDER_ENABLE_TEST=OFF
-#-Dpybind11_DIR=/usr/lib/cmake/pybind11/
+cmake CMakeLists.txt -Dpybind11_DIR=/usr/lib/cmake/pybind11/
 make
 ctest . --output-on-failure 
 out=$?
