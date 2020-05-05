c29d0193e7bb5b718b9cfb1ebd396a7b43593be6
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-05-05 11:25:19 +0200

    fix def

--- source/util.hpp
+++ source/util.hpp
@@ -36,7 +36,7 @@ void replace(std::string &r, std::string const & from, std::string const &to);
 std::string replace_(std::string const &s, std::string const & from, std::string const &to);
 
 /// Move all occurances of non-alphanumerical characters to the end of the string and replace them with their ASCII codes.
-std::string to_alphanumerical(const std::string& input);
+std::string to_alphanumerical(std::string const & input);
 
 /// check if string begins with given prefix
 bool begins_with(std::string const &source, std::string const &prefix);
