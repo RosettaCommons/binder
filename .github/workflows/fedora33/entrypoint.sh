2d878790f0cd0b316eb73cf70d4c3b3888f41cf7
Andrii Verbytskyi <andriish@rhel8.mpp.mpg.de>
2020-08-12 15:01:35 +0200

    Remove debug for Fedora

--- .github/workflows/fedora33/entrypoint.sh
+++ .github/workflows/fedora33/entrypoint.sh
@@ -13,6 +13,4 @@ ldd -u -r source/binder
 ctest . --output-on-failure 
 out=$?
 cat Testing/Temporary/LastTest.log
-cat test/T42_stl_names.cpp
-cat test/T43_stl_names.cpp
 echo ::set-output name=out::$out
