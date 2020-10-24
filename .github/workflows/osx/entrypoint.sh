b1e429cce0d922821cf21fb73a70368f71e091ab
Andrii Verbytskyi <andriish@mppw40-138.mppmu.mpg.de>
2020-10-24 08:35:55 -0700

    Try to fix CI

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -4,9 +4,6 @@ brew install wget coreutils xz
 wget --no-verbose https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-x86_64-apple-darwin.tar.xz
 ls
 tar -xJf clang+llvm-11.0.0-x86_64-apple-darwin.tar.xz
-#xz -d clang+llvm-11.0.0-x86_64-apple-darwin.tar.xz
-#ls
-#tar -x clang+llvm-11.0.0-x86_64-apple-darwin.tar
 export PATH=$PATH:clang+llvm-11.0.0-x86_64-apple-darwin/bin
 
 cmake CMakeLists.txt
