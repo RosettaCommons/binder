22afcb5825448b5e25dd77d1d616281218c7b914
Andrii Verbytskyi <andriish@mppw40-138.mppmu.mpg.de>
2020-10-24 08:46:11 -0700

    Try to fix CI

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 set -x
-brew install wget coreutils xz
+brew install wget coreutils xz pybind11
 wget --no-verbose https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-x86_64-apple-darwin.tar.xz
 ls
 tar -xJf clang+llvm-11.0.0-x86_64-apple-darwin.tar.xz
