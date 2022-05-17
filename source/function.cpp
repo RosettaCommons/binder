103bf4ea46005a02b881598dbc187108b7251443
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2022-05-17 15:32:44 +0200

    python_function_name: no else after return

--- source/function.cpp
+++ source/function.cpp
@@ -190,17 +190,15 @@ string template_specialization(FunctionDecl const *F)
 string python_function_name(FunctionDecl const *F)
 {
 	if( F->isOverloadedOperator() ) return cpp_python_operator_map.at( F->getNameAsString() );
-	else {
-		// if( auto m = dyn_cast<CXXMethodDecl>(F) ) {
-		// }
-		// else{
-		// 	if( F->getTemplatedKind() == FunctionDecl::TK_MemberSpecialization  or   F->getTemplatedKind() == FunctionDecl::TK_FunctionTemplateSpecialization ) outs() << namespace_from_named_decl(F) << "::" << F->getNameAsString() << "\n";
-		// }
+	// if( auto m = dyn_cast<CXXMethodDecl>(F) ) {
+	// }
+	// else{
+	// 	if( F->getTemplatedKind() == FunctionDecl::TK_MemberSpecialization  or   F->getTemplatedKind() == FunctionDecl::TK_FunctionTemplateSpecialization ) outs() << namespace_from_named_decl(F) << "::" << F->getNameAsString() << "\n";
+	// }
 
-		// there is no point of generating different names for different template function since in most cases we can treat them as overload's
-		// return mangle_type_name( F->getNameAsString() + template_specialization(F) );
-		return F->getNameAsString();
-	}
+	// there is no point of generating different names for different template function since in most cases we can treat them as overload's
+	// return mangle_type_name( F->getNameAsString() + template_specialization(F) );
+	return F->getNameAsString();
 }
 
 // Generate function pointer type string for given function: void (*)(int, doule)_ or  void (ClassName::*)(int, doule)_ for memeber function
