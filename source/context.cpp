05da4417f932ad7b4b62341e12f9b579ab7e5419
andriish <andriish@users.noreply.github.com>
2024-04-04 20:51:33 +0200

    Update context.cpp use empty()
    Update context.cpp use empty() to avoid clang-tidy warnings

--- source/context.cpp
+++ source/context.cpp
@@ -116,7 +116,7 @@ PYBIND11_MODULE({2}, root_module) {{
 
 	std::vector< std::pair<std::string, std::string> > sub_modules {{
 {3}	}};
-	for(auto &p : sub_modules ) modules[p.first.size() ? p.first+"::"+p.second : p.second] = modules[p.first].def_submodule( mangle_namespace_name(p.second).c_str(), ("Bindings for " + p.first + "::" + p.second + " namespace").c_str() );
+	for(auto &p : sub_modules ) modules[ !p.first.empty() ? p.first+"::"+p.second : p.second] = modules[p.first].def_submodule( mangle_namespace_name(p.second).c_str(), ("Bindings for " + p.first + "::" + p.second + " namespace").c_str() );
 
 	//pybind11::class_<std::shared_ptr<void>>(M(""), "_encapsulated_data_");
 
