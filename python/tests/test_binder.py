aa1ec55ea1839658ab3a5cfca5a7739fc196d10a
Hailin Wang <hailin.wang@connect.polyu.hk>
2024-06-23 19:01:52 +0800

    Do not check binder version

--- python/tests/test_binder.py
+++ python/tests/test_binder.py
@@ -1,9 +1,6 @@
 import re
 import subprocess
 
-from binder import __version__
-from packaging.version import Version
-
 
 def test_check_binder_llvm_version():
     # Check the output of the `binder -version` command to obtain the versions of Binder and LLVM
@@ -11,7 +8,5 @@ def test_check_binder_llvm_version():
     pattern = r"binder ([\d\.]+)\nLLVM ([\d\.]+)"
     binder_version, llvm_version = re.match(pattern, output).groups()
 
-    # Check the versions of Binder and LLVM
-    binder_base_version = Version(__version__).base_version
-    assert binder_base_version == binder_version, f"Binder version mismatch: {binder_base_version} != {binder_version}"
+    # Check the versions of LLVM
     assert llvm_version == "6.0.1", f"LLVM version mismatch: {llvm_version} != 6.0.1"
