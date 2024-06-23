9c348cecf137ad74df16d7fedf76e360e505e533
Hailin Wang <hailin.wang@connect.polyu.hk>
2024-06-23 18:30:03 +0800

    Update version scheme to no-guess-dev

--- python/binder/__init__.py
+++ python/binder/__init__.py
@@ -7,15 +7,14 @@ try:
 except ImportError:
     _default_version = "0.0.1"
 
-
 __all__ = ["binder", "__version__"]
-
 
 BINDER_BIN_DIR = os.path.join(os.path.dirname(__file__), "bin")
 
 
 def binder():
     raise SystemExit(subprocess.call([os.path.join(BINDER_BIN_DIR, "binder")] + sys.argv[1:], close_fds=False))
+
 
 def _get_version():
     """Return the version string used for __version__."""
@@ -29,12 +28,13 @@ def _get_version():
 
             return setuptools_scm.get_version(
                 root=root,
-                version_scheme="post-release",
+                version_scheme="no-guess-dev",
                 fallback_version=_default_version,
             )
         except (ImportError, LookupError):
             return _default_version
     else:  # Get the version from the _version.py setuptools_scm file.
         return _default_version
+
 
 __version__ = _get_version()
