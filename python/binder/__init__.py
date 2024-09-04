d0d9f7bf1619e3708149c02cde014997ca1da47e
Hailin Wang <hailin.wang@connect.polyu.hk>
2024-09-04 10:13:21 +0800

    Update version configurations

--- python/binder/__init__.py
+++ python/binder/__init__.py
@@ -1,11 +1,7 @@
 import os
-import sys
 import subprocess
-
-try:
-    from ._version import version as _default_version
-except ImportError:
-    _default_version = "0.0.1"
+import sys
+from re import error as RegexError
 
 __all__ = ["binder", "__version__"]
 
@@ -13,28 +9,26 @@ BINDER_BIN_DIR = os.path.join(os.path.dirname(__file__), "bin")
 
 
 def binder():
-    raise SystemExit(subprocess.call([os.path.join(BINDER_BIN_DIR, "binder")] + sys.argv[1:], close_fds=False))
+    raise SystemExit(subprocess.call([os.path.join(BINDER_BIN_DIR, "binder"), *sys.argv[1:]], close_fds=False))
 
 
 def _get_version():
-    """Return the version string used for __version__."""
-    # Only shell out to a git subprocess if really needed, and not on a
-    # shallow clone, such as those used by CI, as the latter would trigger
-    # a warning from setuptools_scm.
     root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
-    if os.path.exists(os.path.join(root, ".git")) and not os.path.exists(os.path.join(root, ".git/shallow")):
+    if os.path.exists(os.path.join(root, ".git")) and not os.path.exists(
+        os.path.join(root, ".git/shallow")
+    ):
         try:
             import setuptools_scm
 
-            return setuptools_scm.get_version(
-                root=root,
-                version_scheme="no-guess-dev",
-                fallback_version=_default_version,
-            )
-        except (ImportError, LookupError):
-            return _default_version
-    else:  # Get the version from the _version.py setuptools_scm file.
-        return _default_version
+            return setuptools_scm.get_version(root=str(root))
+        except (ImportError, RegexError, LookupError):
+            pass
+    try:
+        from ._version import version
+
+        return version
+    except ImportError:
+        return "0.0.1"
 
 
-__version__ = _get_version()
+__version__: str = _get_version()
--- python/binder/__main__.py
+++ python/binder/__main__.py
@@ -1,5 +1,4 @@
 from binder import binder
 
-
 if __name__ == "__main__":
     binder()
